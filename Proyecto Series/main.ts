d95ea8bfc04860d7e52d780ede346358de406c74
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-19 08:28:44 -0500

    cambios

--- Proyecto Series/main.ts
+++ Proyecto Series/main.ts
@@ -1,22 +1,35 @@
 import { Serie } from "./Serie";
 
-const seriesTbody: HTMLElement = document.getElementById('series')!; // Nodo tbody que tiene el id="series"
+import { series } from './data.js';
+
+let seriesTbody: HTMLElement = document.getElementById('series')!;
+const avrgTemps: HTMLElement = document.getElementById("promedio-temporadas")!;
+
+renderSeriesInTable(series);
+
+avrgTemps.innerHTML = `${getAverageSeasons(series)}`
 
 function renderSeriesInTable(series: Serie[]): void {
-  series.forEach(serie => {
+  series.forEach((serie) => {
     let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${serie.id}</td>
-                           <td>${serie.name}</td>
+    trElement.innerHTML = `<td>${serie.name}</td>
                            <td>${serie.channel}</td>
                            <td>${serie.seasons}</td>`;
     seriesTbody.appendChild(trElement);
   });
 
-  function getAverageSeasons(series: Serie[]): number {
-    let totalSeasons: number = 0;
+  const average = getAverageSeasons(series);
+
+  let promedio = document.createElement("tr");
+  promedio.innerHTML = `<td colspan="3">Promedio Temporadas: ${average}</td>`;
+  seriesTbody.appendChild(promedio);
+}
+
+function getAverageSeasons(series: Serie[]): number {
+  let totalSeasons: number = 0;
     let numberSeries: number = 0;
     series.forEach((serie) => totalSeasons = totalSeasons + serie.seasons);
     series.forEach((serie) => numberSeries = numberSeries + 1);
     return totalSeasons;
-  }
 }
+
