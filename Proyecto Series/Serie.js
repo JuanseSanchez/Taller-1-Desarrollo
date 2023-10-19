d46104f71c08a31df418427aa4c02a174163436d
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-19 08:51:46 -0500

    correcion

--- Proyecto Series/Serie.js
+++ Proyecto Series/Serie.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Serie = void 0;
 var Serie = /** @class */ (function () {
-    function Serie(id, nombre, plataforma, temporadas, descripcion, pagina, imagen) {
+    function Serie(id, name, channel, seasons, description, link, image) {
         this.id = id;
         this.name = name;
         this.channel = channel;
